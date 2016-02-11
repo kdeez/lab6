f3747c8a2b2f9b431a3b09d093f371371fd5a070
Kevin Dang <danggkev@gmail.com>
2016-02-11 19:42:17 +0000

    ajax lab

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 var project = require('./routes/project');
+//add variable 
+var palette = require('./routes/palette');
 // Example route
 // var user = require('./routes/user');
 
@@ -38,6 +40,8 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/project/:id', project.projectInfo);
+//add the route
+app.get('/palette', palette.randomPalette);
 // Example route
 // app.get('/users', user.list);
 
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -11,8 +11,11 @@ $(document).ready(function() {
 function initializePage() {
 	$('.project a').click(addProjectDetails);
 
+
+	//randomizes the color 
 	$('#colorBtn').click(randomizeColors);
 }
+
 
 /*
  * Make an AJAX call to retrieve project details and add it in
@@ -23,16 +26,49 @@ function addProjectDetails(e) {
 
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	console.log("/project/" + idNumber);
 	console.log("User clicked on project " + idNumber);
 }
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
+	e.preventDefault();
+
+	$.get("/palette", changeColors);
+
 	console.log("User clicked on color button");
 }
+
+
+
+
+function addProject(result) {
+	console.log(result);
+
+	var projectHTML = 
+	'<img src="' + result.image + '" class="detailsImage">' +
+    '<h2>' + result.title + '</h2>' +
+    '<p><small>' + result.date + '</small></p></a>' +
+    '<br><p>' + result.summary + '</p>';
+	$("#project" + result.id +  " .details").html(projectHTML);
+}
+
+
+
+function changeColors(result) {
+	var color = result.colors.hex;
+	
+	$('body').css('background-color', color[0]);
+	
+	$('.thumbnail').css('background-color', color[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', color[2]);
+	$('p').css('color', color[3]);
+	$('.project img').css('opacity', .75);
+}
+
